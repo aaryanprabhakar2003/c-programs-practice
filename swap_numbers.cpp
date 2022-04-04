c59f11723a6251b286c6f22a49f6ef57e0320d14
aaryan07-ap <aaryanprabhakar07@gmail.com>
2022-04-04 09:06:49 +0530

    program to swap 2 numbers

---
+++ swap_numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=5,b=10,temp;
+    cout<<"before swapping"<<endl;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<"after swaapping"<<endl;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<a<<endl;
+    cout<<b<<endl;
+}
