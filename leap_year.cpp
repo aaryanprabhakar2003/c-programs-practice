2ccf4a6012cb37374f700970b38e5e3d76e1c2f4
aaryan07-ap <aaryanprabhakar07@gmail.com>
2022-04-04 09:19:56 +0530

    program to find leap year

---
+++ leap_year.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int year;
+    cout<<"enter year:"<<endl;
+    cin >>year;
+    if ((year%4 == 0 && year %100!=0)||year%400==0){
+        cout<<"it is a leap year"<<endl;
+    }
+    else{
+        cout<<"it is not a leap year"<<endl;
+    }
+}
