0012f00258eee8c910ab6e165c7959ef04458a76
Aaryan Prabhakar <aaryanprabhakar07@gmail.com>
2022-04-10 10:00:55 +0530

    Create calculator_switch.cpp
    calculator built using switch statement in c++

---
+++ calculator_switch.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int add,sub,mul,div,n1,n2,opt,res;
+    cout<<"select the option from the menu"<<endl;
+    cout<<"1.  addition"<<endl;
+    cout<<"2.  subtraction"<<endl;
+    cout<<"3. multiplication"<<endl;
+    cout<<"4. division"<<endl;
+    cin >> opt;
+    switch(opt){
+    case 1:
+    cout<<"enter the first number"<<endl;
+    cin>>n1;
+    cout<<"enter the second number"<<endl;
+    cin>>n2;
+    res=n1+n2;
+    cout<<res<<endl;
+    break;
+
+    case 2:
+    cout<<"enter the first number"<<endl;
+    cin>>n1;
+    cout<<"enter the second number"<<endl;
+    cin>>n2;
+    res=n1-n2;
+    cout<<res<<endl;
+    break;
+    case 3:
+    cout<<"enter the first number"<<endl;
+    cin>>n1;
+    cout<<"enter the second number"<<endl;
+    cin>>n2;
+    res=n1*n2;
+    cout<<res<<endl;
+    break;
+    case 4:
+    cout<<"enter the first number"<<endl;
+    cin>>n1;
+    cout<<"enter the second number"<<endl;
+    cin>>n2;
+    res=n1/n2;
+    cout<<res<<endl;
+    break;
+    }
+}
+
