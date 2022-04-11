a3d8e639f64aafdf0c577b70ef13b02c5ebefca4
Aaryan Prabhakar <aaryanprabhakar07@gmail.com>
2022-04-11 11:58:26 +0530

    added details for 2 students and compare their marks

--- struccture.cpp
+++ struccture.cpp
@@ -8,13 +8,23 @@ struct student{
 
 int main(){
     student s1;
-    cout<<"enter details for the student";
+    student s2;
+    cout<<"enter details for the 1st student"<<endl;
     cin>>s1.name;
     cin>>s1.roll_no;
     cin>>s1.marks;
-    cout<<s1.name<<endl;
+    cout<<"enter details for the 2nd student"<<endl;
+    cin>>s2.name;
+    cin>>s2.roll_no;
+    cin>>s2.marks;
+    /*cout<<s1.name<<endl;
     cout<<s1.roll_no<<endl;
-    cout<<s1.marks<<endl;
-
+    cout<<s1.marks<<endl;*/
+    if(s1.marks>s2.marks){
+        cout<<s1.name<<" has more marks than "<<s2.name<<endl;
+    }
+    else{
+        cout<<s2.name<<" has more marks than "<<s1.name<<endl;
+    }
 
 }
