0cbf228065b29476e40a7310f5a328516f784f4a
Aaryan Prabhakar <aaryanprabhakar07@gmail.com>
2022-05-03 15:45:42 +0530

    Update operator overloading.cpp

--- operator overloading.cpp
+++ operator overloading.cpp
@@ -16,8 +16,8 @@ int main(){
     counter c1,c2;
     c1.increment();//increment c1 value by 1
     c2.decrement();// decrement c2 value by 1
-    c1.disp();
-    c2.disp();
+    c1.disp();//display c1 value
+    c2.disp();//display c2 value
 };*/
 
 
@@ -37,18 +37,18 @@ int main(){
     counter c1,c2;
     c1.operator ++();//increment c1 value by 1
     c2.operator --();//decrement c2 value by 1
-    c1.disp();
-    c2.disp();
+    c1.disp();// display c1 value by 1
+    c2.disp();//display c2 value by 1
 };*/
 
-class counter{
+/*class counter{
     int count;
     public:
     counter()
     {count=0;}
-    void operator ++()
+    void operator ++()//post-increment
     {count++;}
-    void operator --()
+    void operator --()//post-decrement
     {count--;}
     void disp()
     {cout<<count<<endl;}
@@ -57,6 +57,46 @@ int main(){
     counter c1,c2;
     c1.operator ++();//increment c1 value by 1
     c2.operator --();//decrement c2 value by 1
+    c1.disp();//display c1 value
+    c2.disp();// disp c2 value by 1
+}*/
+
+class counter{
+    int count;
+    public:
+    counter()
+    {count=0;}
+    counter operator ++(){
+        counter temp;
+        temp.count=++count;
+        return temp;
+    };
+    counter operator ++(int){
+        counter temp;
+        temp.count=count++;
+        return temp;
+    }
+    counter operator --(){
+        counter temp;
+        temp.count=--count;
+        return temp;
+    }
+    counter operator --(int){
+        counter temp;
+        temp.count=count--;
+        return temp;
+    }
+    void disp()
+    {cout<<count<<endl;}
+};
+int main(){
+    counter c1,c2;
+    //c1.operator ++();
+    c1.operator --();
+    c2.operator ++();
+    c2.operator ++();
+    
     c1.disp();
+    c2.disp();
     c2.disp();
 }
